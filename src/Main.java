91ed0399f372169b86225eac8f069a328a0cd9d4
Natali <Tasha-T89@mail.ru>
2022-09-29 21:05:09 +0300

    Lambda commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -10,7 +11,20 @@ public class Main {
                 new Person("Sergey", "Popov", 17),
                 new Person("Oleg", "Boykov-Suslikov-Bob", 13)
         ));
-        Collections.sort(notables, new ComparePerson());
+        Comparator<Person> compPerson = (p1, p2) -> {
+            String[] perOneSurname = p1.getSurname().split("-");
+            String[] perTwoSurname = p2.getSurname().split("-");
+            if (perOneSurname.length > perTwoSurname.length) {
+                return -1;
+            } else if (perOneSurname.length < perTwoSurname.length) {
+                return 1;
+            } else return Integer.compare(p2.getAge(), p1.getAge());
+        };
+
+        Collections.sort(notables, compPerson);
+
         System.out.println(notables);
     }
 }
+
+
